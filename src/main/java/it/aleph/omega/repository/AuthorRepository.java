d6222aca5d7e17cc87664f67de5e2f88bd979453
Nicola Rossi <nikolajbogdanorov@gmail.com>
2023-03-27 21:18:41 +0200

    Task 46 - Added Repositories

--- src/main/java/it/aleph/omega/repository/AuthorRepository.java
+++ src/main/java/it/aleph/omega/repository/AuthorRepository.java
@@ -1,9 +1,9 @@
 package it.aleph.omega.repository;
 
-import it.aleph.omega.model.Book;
+import it.aleph.omega.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
